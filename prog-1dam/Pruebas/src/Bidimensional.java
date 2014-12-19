4114cea6648dcdd0c14c0a9326dae27a4196166d
unknown <amacias@AULA07-22.aulas.triana>
2014-12-19 10:29:11 +0100

    19-12

--- prog-1dam/Pruebas/src/Bidimensional.java
+++ prog-1dam/Pruebas/src/Bidimensional.java
@@ -24,76 +24,93 @@ public class Bidimensional {
 			}
 		}
 		
-		System.out.println("Hola amigo, dime qué casilla abrimos");
-		fila1 = Leer.datoInt();
-		columna1 = Leer.datoInt();
-		mostrar [fila1-1][columna1-1] = 1;
+		do {
+		
+			System.out.println("Hola amigo, dime qué casilla abrimos");
+			System.out.print("\nIntroduzca la fila: ");
+			fila1 = Leer.datoInt();
+			System.out.print("\nIntroduzca la columna: ");
+			columna1 = Leer.datoInt();
+			mostrar [fila1-1][columna1-1] = 1;
+				
 			
-		
-		// Inicio del dibujo del tablero
-		
-		for (int i = 0; i < ncolumnas + 2; i++) {    // Para el número de columnas, más la inicio y fin
-			System.out.print("*\t");       // Dibuja un asterisco (fila de asteriscos)
-		}
-		System.out.println(" ");
-		for (int i = 0; i < tablero.length; i++) {		
-			System.out.print("*\t");  // Asterisco al principio de cada fila
-			for (int j = 0; j < tablero[i].length; j++) {
-				if (mostrar [i][j] == 0) {
-					System.out.print("-\t");
-				} else
-					System.out.print(tablero[i][j]+"\t");
-				if (j == tablero[i].length-1)   // Comprueba si hemos puesto el último de la fila
-					System.out.print("*");  // Y en caso afirmativo le ponemos asterisco
+			// Inicio del dibujo del tablero
+			
+			for (int i = 0; i < ncolumnas + 2; i++) {    // Para el número de columnas, más la inicio y fin
+				System.out.print("*\t");       // Dibuja un asterisco (fila de asteriscos)
 			}
-			System.out.print("\n");   // Salto de linea cada vez que hay que cambiar de i (nueva fila)
-		}
-		for (int i = 0; i < ncolumnas + 2; i++) {    // Para el número de columnas, más la inicio y fin
-			System.out.print("*\t");       // Dibuja un asterisco (fila de asteriscos)
-		}
-		
-		// Fin del tablero
-		
-		System.out.println("\n\nHola amigo, dime qué casilla abrimos ahora");
-		fila2 = Leer.datoInt();
-		columna2 = Leer.datoInt();
-		mostrar [fila2-1][columna2-1] = 1;
-		
-		// Inicio del dibujo del tablero
-		
-				for (int i = 0; i < ncolumnas + 2; i++) {    // Para el número de columnas, más la inicio y fin
-					System.out.print("*\t");       // Dibuja un asterisco (fila de asteriscos)
+			System.out.println(" ");
+			for (int i = 0; i < tablero.length; i++) {		
+				System.out.print("*\t");  // Asterisco al principio de cada fila
+				for (int j = 0; j < tablero[i].length; j++) {
+					if (mostrar [i][j] == 0) {
+						System.out.print("-\t");
+					} else
+						System.out.print(tablero[i][j]+"\t");
+					if (j == tablero[i].length-1)   // Comprueba si hemos puesto el último de la fila
+						System.out.print("*");  // Y en caso afirmativo le ponemos asterisco
 				}
-				System.out.println(" ");
-				for (int i = 0; i < tablero.length; i++) {		
-					System.out.print("*\t");  // Asterisco al principio de cada fila
-					for (int j = 0; j < tablero[i].length; j++) {
-						if (mostrar [i][j] == 0) {
-							System.out.print("-\t");
-						} else
-							System.out.print(tablero[i][j]+"\t");
-						if (j == tablero[i].length-1)   // Comprueba si hemos puesto el último de la fila
-							System.out.print("*");  // Y en caso afirmativo le ponemos asterisco
+				System.out.print("\n");   // Salto de linea cada vez que hay que cambiar de i (nueva fila)
+			}
+			for (int i = 0; i < ncolumnas + 2; i++) {    // Para el número de columnas, más la inicio y fin
+				System.out.print("*\t");       // Dibuja un asterisco (fila de asteriscos)
+			}
+			
+			// Fin del tablero
+			
+			System.out.println("\n\nHola amigo, dime qué casilla abrimos ahora");
+			System.out.print("\nIntroduzca la fila: ");
+			fila2 = Leer.datoInt();
+			System.out.print("\nIntroduzca la columna: ");
+			columna2 = Leer.datoInt();
+			mostrar [fila2-1][columna2-1] = 1;
+			
+			// Inicio del dibujo del tablero
+			
+					for (int i = 0; i < ncolumnas + 2; i++) {    // Para el número de columnas, más la inicio y fin
+						System.out.print("*\t");       // Dibuja un asterisco (fila de asteriscos)
 					}
-					System.out.print("\n");   // Salto de linea cada vez que hay que cambiar de i (nueva fila)
+					System.out.println(" ");
+					for (int i = 0; i < tablero.length; i++) {		
+						System.out.print("*\t");  // Asterisco al principio de cada fila
+						for (int j = 0; j < tablero[i].length; j++) {
+							if (mostrar [i][j] == 0) {
+								System.out.print("-\t");
+							} else
+								System.out.print(tablero[i][j]+"\t");
+							if (j == tablero[i].length-1)   // Comprueba si hemos puesto el último de la fila
+								System.out.print("*");  // Y en caso afirmativo le ponemos asterisco
+						}
+						System.out.print("\n");   // Salto de linea cada vez que hay que cambiar de i (nueva fila)
+					}
+					for (int i = 0; i < ncolumnas + 2; i++) {    // Para el número de columnas, más la inicio y fin
+						System.out.print("*\t");       // Dibuja un asterisco (fila de asteriscos)
+					}
+					
+					// Fin del tablero
+					
+					boolean acertado = false;
+					if (tablero[fila1-1][columna1-1] == tablero[fila2-1][columna2-1]) {
+						System.out.println("\nAcierto!");
+						acertado = true;
+						mostrar[fila1-1][columna1-1] = 1;
+						mostrar[fila2-1][columna2-1] = 1;
+					}
+					else {
+						System.out.println("\nError!!");
+					}
+						
+					System.out.println("\nPulsa la tecla A para continuar");
+					tecla = Leer.datoChar();
+				
+		
+				
+				if ((tecla == 'a' || tecla == 'A') && acertado) {
+					for (int i = 0; i < 50; i++) 
+						System.out.println();
 				}
-				for (int i = 0; i < ncolumnas + 2; i++) {    // Para el número de columnas, más la inicio y fin
-					System.out.print("*\t");       // Dibuja un asterisco (fila de asteriscos)
-				}
 				
-				// Fin del tablero
-				
-				
-				if (tablero[fila1-1][columna1-1] == tablero[fila2-1][columna2-1])
-					System.out.println("\nOle tu chocho");
-				else
-					System.out.println("\nAjajaja la pelas");
-				
-				System.out.println("\nPulsa la tecla A para continuar");
-				tecla = Leer.datoChar();
-				
-				if (tecla == 'a' || tecla == 'A')
-					for (int i = 0; i < 50; ++i) System.out.println();
+		} while (tecla == 'a' || tecla == 'A');		
 		
 	}
 
